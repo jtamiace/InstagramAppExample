6ef800be05b5d4a22ff368ce01ca9cdd68718a56
jtamiace <jtamiace@gmail.com>
2014-04-08 17:28:31 -0700

    instagram App

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 //dependencies for each module used
+var dotenv = require('dotenv');
 var express = require('express');
 var http = require('http');
+var ig = require('instagram-node-lib');
 var path = require('path');
 var handlebars = require('express3-handlebars');
+var hashtag = require('./routes/hashtag');
 var app = express();
 //route for hashtag
 var hashtag = require('./routes/hashtag');
 var index = require('./routes/index');
 //database setup
+var models = require('../models');
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/instagramexample');
 
@@ -26,8 +30,28 @@ app.get('/hashtag', function (req, res) {
 app.post('/hashtag', hashtag.getHashtag);
 app.post('/save', hashtag.saveFavorites);
 app.post('/delete', index.deleteImage);
+app.post('hashtag', hashtag.getHashtag);
 //set environment ports and start application
 app.set('port', process.env.PORT || 3000);
+app.use(express.bodyParser());
+app.use(express.static(path.join(_dirname, 'public')));
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
+});
+exports.deleteImage = function(req, res) {
+	models.Img.find({ _id: req.body.id }).remove().exec();
+	res.redirect('/');
+}
+
+//load environment variables
+dotenv.load();
+
+//add instagram api setup
+ig.set('client_id', process.env.instagram_client_id);
+ig.set('client_secret', process.env.instagram_client_secret);
+ig.tags.info({
+	name: 'sushi',
+	complete: function(data) {
+		console.log(data);
+	}
 });
